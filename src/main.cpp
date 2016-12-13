1eb41fc0c8ce34626f708dfb0ef8ea4381e34aaf
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2016-12-13 21:15:14 +0500

    rfct

--- src/main.cpp
+++ src/main.cpp
@@ -23,27 +23,28 @@ void initDb()
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("tasks.db");
 
-    if (db.open()) {
-        QSqlQuery q;
-
-        int p = -1, t = -1;
-
-        if (q.exec("SELECT count(*) FROM people")) {
-            q.first();
-            p = q.value(0).toInt();
-        } else {
-            qDebug() << "ERR: Query failed\n" << q.lastError();
-        }
-
-        if (q.exec("SELECT count(*) FROM tasks")) {
-            q.first();
-            t = q.value(0).toInt();
-        } else {
-            qDebug() << "ERR: Query failed\n" << q.lastError();
-        }
-
-        qDebug("OK: Connected to DB\n%d people and %d tasks registered", p, t);
-    } else {
+    if (!db.open()) {
         qDebug() << "ERR: Connection to DB failed\n%s" << db.lastError();
+        return;
     }
+
+    QSqlQuery q;
+
+    int p = -1, t = -1;
+
+    if (q.exec("SELECT count(*) FROM people")) {
+        q.first();
+        p = q.value(0).toInt();
+    } else {
+        qDebug() << "ERR: Query failed\n" << q.lastError();
+    }
+
+    if (q.exec("SELECT count(*) FROM tasks")) {
+        q.first();
+        t = q.value(0).toInt();
+    } else {
+        qDebug() << "ERR: Query failed\n" << q.lastError();
+    }
+
+    qDebug("OK: Connected to DB\n%d people and %d tasks registered", p, t);
 }
